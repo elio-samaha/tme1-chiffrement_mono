de3a947367d3ba52757fe59bee1f4aff8b21405d
SAMAHA <elio.samaha@etu.sorbonne-universite.fr>
2024-02-02 17:49:41 +0100

    read csv

--- crypt_auto_mono.py
+++ crypt_auto_mono.py
@@ -3,6 +3,9 @@
 import sys
 import random
 import operator
+from collections import Counter, defaultdict
+import json
+import csv
 
 #Exo 3)1 :
 
@@ -15,20 +18,53 @@ import operator
 # Where file contains the ciphertext
 # It is recommended to write a few functions for this exercise
 
-# Assume we are working with an uppercase alphabet
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+ciphertext = ""
+ciphertext_eval = 0
+encryption_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+plaintext = ""
+plaintext_eval = 0
+decryption_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+iter = 0
+
+nb_tetra = defaultdict(lambda : 0.001)
+with open("nb_tetra_fr.csv" , "r") as file:
+    for line in file:
+        parts = line.strip().split(';')
+        if len(parts) == 2 and parts[0].isalpha() and len(parts[0]) == 4:
+            nb_tetra[parts[0]] = int(parts[1])
+
+
+print((nb_tetra["aebsc"]))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit()
+    file_name = sys.argv[1]
+
+    # Do not modify these lines except for variable names
+    print ("texte chiffré\n" + ciphertext)
+    print ("évaluation " + str(ciphertext_eval))
+    print ("\nAprès " + str(iter) + " itérations, texte déchiffré\n" + plaintext)
+    print ("substitution appliquée au texte fourni " + encryption_key)
+    print ("clef " + decryption_key)
+    print ("évaluation " + str(plaintext_eval))
+
+
+
+"""
 
 def decrypt(ciphertext, key):
-    """
-    Decrypt the ciphertext using the key.
-    """
+"""
+    #Decrypt the ciphertext using the key.
+"""
     key_map = {key[i]: ALPHABET[i] for i in range(len(ALPHABET))}
     return ''.join(key_map.get(c, c) for c in ciphertext)
 
 def evaluate(text, tetragram_freqs):
-    """
-    Evaluate the quality of the decrypted text using tetragram frequencies.
-    """
+"""
+    #Evaluate the quality of the decrypted text using tetragram frequencies.
+"""
     score = 0
     for i in range(len(text) - 3):
         tetragram = text[i:i+4]
@@ -37,9 +73,9 @@ def evaluate(text, tetragram_freqs):
     return score
 
 def load_tetragram_frequencies(filename):
-    """
-    Load the tetragram frequencies from the given file.
-    """
+"""
+    #Load the tetragram frequencies from the given file.
+"""
     frequencies = {}
     with open(filename, 'r') as file:
         for line in file:
@@ -49,9 +85,9 @@ def load_tetragram_frequencies(filename):
     return frequencies
 
 def swap_two_letters(key):
-    """
-    Swap two random letters in the decryption key.
-    """
+"""
+    #Swap two random letters in the decryption key.
+"""
     key = list(key)
     a, b = random.sample(range(len(key)), 2)
     key[a], key[b] = key[b], key[a]
@@ -87,14 +123,4 @@ for iter in range(max_iterations):
         plaintext = new_plaintext
         plaintext_eval = new_eval
 
-
-
-
-# Do not modify these lines except for variable names
-print ("texte chiffré\n" + ciphertext)
-print ("évaluation " + str(ciphertext_eval))
-print ("\nAprès " + str(iter) + " itérations, texte déchiffré\n" + plaintext)
-print ("substitution appliquée au texte fourni " + encryption_key)
-print ("clef " + decryption_key)
-print ("évaluation " + str(plaintext_eval))
-
+"""
